1489d247ced105d9d92d5452c3aab9de6263b95b
jncodes <neeraja.jayaraman@gmail.com>
2021-04-01 00:08:19 -0700

    Modified election audit code

--- pypoll.py
+++ pypoll.py
@@ -66,17 +66,17 @@ with open(file_to_load) as election_data:
         # Print out each candidate's name, vote count, and percentage of votes to the terminal.
         print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
 
-        # Determine winning vote count and candidate
+    # Determine winning vote count and candidate
         if (votes>winning_count) and (vote_percentage>winning_percentage):
             winning_count = votes
             winning_percentage = vote_percentage
             winning_candidate = candidate_name
    
-        #print out the winning candidate, vote count and percentage to terminal
-        winning_candidate_summary = (
-            f"-------------------------\n"
-            f"Winner: {winning_candidate}\n"
-            f"Winning Vote Count: {winning_count:,}\n"
-            f"Winning Percentage: {winning_percentage:.1f}%\n"
-            f"-------------------------\n")
-        print(winning_candidate_summary)
+#print out the winning candidate, vote count and percentage to terminal
+winning_candidate_summary = (
+    f"-------------------------\n"
+    f"Winner: {winning_candidate}\n"
+    f"Winning Vote Count: {winning_count:,}\n"
+    f"Winning Percentage: {winning_percentage:.1f}%\n"
+    f"-------------------------\n")
+print(winning_candidate_summary)
